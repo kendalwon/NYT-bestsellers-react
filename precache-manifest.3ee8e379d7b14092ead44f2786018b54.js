944a649bb8a31b0d1d6b29c91f0e0742f87c1b37
kendalwon <kendalwon@gmail.com>
2020-07-10 13:48:29 -0400

    Updates

---
+++ precache-manifest.3ee8e379d7b14092ead44f2786018b54.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e3b76b0c1149d7bac7aa8b1b80cb542",
+    "url": "/NYT-bestsellers-react/index.html"
+  },
+  {
+    "revision": "8d27bf215ec5aefa4ea1",
+    "url": "/NYT-bestsellers-react/static/css/main.be40f2dc.chunk.css"
+  },
+  {
+    "revision": "0acc9b1906a89b380f9e",
+    "url": "/NYT-bestsellers-react/static/js/2.149debb4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/NYT-bestsellers-react/static/js/2.149debb4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8d27bf215ec5aefa4ea1",
+    "url": "/NYT-bestsellers-react/static/js/main.48d57808.chunk.js"
+  },
+  {
+    "revision": "6692e1d48c66b275cb76",
+    "url": "/NYT-bestsellers-react/static/js/runtime-main.55fa5394.js"
+  },
+  {
+    "revision": "281336652a322de2de2b90ba57ddeafa",
+    "url": "/NYT-bestsellers-react/static/media/Chomsky.28133665.otf"
+  }
+]);
